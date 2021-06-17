e9d613151d07e90382e95f44495315c2868ea335
Vinicius <vdavidmarques@gmail.com>
2021-06-16 21:40:36 -0300

    novos files

--- article.php
+++ article.php
@@ -6,56 +6,56 @@
 
         <div class="row row-cols-1 row-cols-md-3">
             <div class="card bg-color-1" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="img/passos.png" class="card-img-top" alt="Primeiros passos">
                 <div class="card-body">                    
                     <p class="card-text"><strong>Primeiros passos:</strong> aprenda como começar do jeito certo e definir sua PUV (Proposta Única de Valor). Acredite: deixar isso para depois vai dar uma dor de cabeça desnecessária!</p>                   
                 </div>            
             </div>
             <div class="card bg-color-2" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="img/formalizacao.png" class="card-img-top" alt="Formalização">
                 <div class="card-body">                    
                     <p class="card-text"><strong>Formalização: MEI ou ME:</strong>Saiba formalizar sua agência para não ter problemas futuros. Entenda os processos, quais são os deveres e obrigações de cada modelo.</p>                   
                 </div>
             </div>
             
             <div class="card bg-color-3" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="img/primeiros-clientes.png" class="card-img-top" alt="Primeiros Clientes">
                 <div class="card-body">                    
-                    <p class="card-text"><strong>Como adquirir os primeiros clientes:</strong>conheça recomendações padrões (e as que estão fora da curva também) para encontrar e conquistar os seus primeiros clientes.</p>                                
+                    <p class="card-text"><strong>Como adquirir os primeiros clientes:</strong> conheça recomendações padrões (e as que estão fora da curva também) para encontrar e conquistar os seus primeiros clientes.</p>                                
                 </div>            
             </div>
         </div> 
         
         <div class="row row-cols-1 row-cols-md-2">
             <div class="card bg-color-1" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="img/prospects.png" class="card-img-top" alt="prospects">
                 <div class="card-body">                    
-                    <p class="card-text"><strong>Como lidar com as objeções dos prospects:</strong>a vida não é feita de “SIM”. Saiba como contornar as situações mais adversas com os prospects e priorize a saúde do seu negócio.</p>                                
+                    <p class="card-text"><strong>Como lidar com as objeções dos prospects:</strong> a vida não é feita de “SIM”. Saiba como contornar as situações mais adversas com os prospects e priorize a saúde do seu negócio.</p>                                
                 </div>            
             </div>
             <div class="card bg-color-2" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="img/propostas.png" class="card-img-top" alt="propostas">
                 <div class="card-body">                    
-                    <p class="card-text"><strong>Prospecção ativa e propostas comerciais:</strong>fale com quem precisa de fato da sua agência, poupe tempo e faça boas propostas, com ofertas justas para todos os envolvidos.</p>                                
+                    <p class="card-text"><strong>Prospecção ativa e propostas comerciais:</strong> fale com quem precisa de fato da sua agência, poupe tempo e faça boas propostas, com ofertas justas para todos os envolvidos.</p>                                
                 </div>            
             </div>
         </div>
 
         <div class="row row-cols-1 row-cols-md-3">
             <div class="card bg-color-1" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="img/clientes-toxicos.png" class="card-img-top" alt="clientes toxicos">
                 <div class="card-body">                    
                     <p class="card-text"><strong>Cuidados com clientes tóxicos:</strong>existem clientes que só geram problemas para a sua agência e equipe, aprenda como identificar eles e não cair nessa armadilha.</p>                                
                 </div>            
             </div>
             <div class="card bg-color-2" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="img/agencia.png" class="card-img-top" alt="agência">
                 <div class="card-body">                    
                     <p class="card-text"><strong>Eugência x Agência:</strong>saiba quando e como deixar de ser uma “eugência” (trabalhar sozinho/a), para montar um negócio com funcionários, processos e uma boa gestão.</p>                                
                 </div>            
             </div>
             <div class="card bg-color-3" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="img/metas-crescimento.png" class="card-img-top" alt="metas e crescimento">
                 <div class="card-body">                    
                     <p class="card-text"><strong>Metas e crescimento:</strong>comece, desde já, a criar um plano de metas que ajude a escalar a sua agência digital. Conheça o caminho que vai te levar ao sucesso!</p>                                            
                 </div>
@@ -64,13 +64,13 @@
     </div>    
 </article>
 
-<section class="mt-5 mb-5 bg-color-1">    
+<section class="mt-5 mb-5 bg-color-2 text-white">    
     <div class="container d-flex align-items-center justify-content-center">
-        <div class="col-sm-6 col-12">
+        <div class="col-md-6 col-12">
             <img src="img/fabio-ricotta.png" alt="Fábio" class="w-100">
         </div>
 
-        <div class="col-sm-6 col-12 m">
+        <div class="col-md-6 col-12 m">
             <h3 class="mb-5">Quer saber com quem você vai aprender?</h3>
             <p>Fábio Ricotta</strong> é <strong>CEO da Agência Mestre</strong> , eleita a Agência do Ano por quatro anos consecutivos (2020, 2019, 2018 e 2017) e primeira Agência Diamond da Resultados Digitais.</p>
             <p>Como mentor e criador do treinamento <strong>Agência10x </strong>, já ensinou mais de 500 donos de agências, freelancers e consultores a gerir o próprio negócio, aumentar as vendas e, de fato, lucrar com a prestação de serviços digitais.</p>
